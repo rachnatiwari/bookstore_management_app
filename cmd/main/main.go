d9d4f7436d6d6c8c14e1771f71d76a1d161779ee
Rachna Tiwari <rachnatiwari0101@gmail.com>
2023-09-16 23:47:42 +0530

    Bookstore Management App

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -4,7 +4,6 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/go-openapi/runtime/middleware"
 	"github.com/gorilla/mux"
 	"github.com/rachnatiwari/bookstore_management_app/pkg/routes"
 )
@@ -12,10 +11,6 @@ import (
 func main() {
 	r := mux.NewRouter()
 	routes.BooksRoute(r)
-	r.Handle("/swagger_main.yml", http.FileServer(http.Dir("./")))
-	opts := middleware.SwaggerUIOpts{SpecURL: "swagger_main.yml"}
-	sh := middleware.SwaggerUI(opts, nil)
-	r.Handle("/docs", sh)
 	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe("localhost:3000", r))
 }
